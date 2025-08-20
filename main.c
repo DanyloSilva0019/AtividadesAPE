12e2439635e22b1f0c9aba41201128b74d83cb9b
DanyloSilva0019 <danylorodriques71@gmail.com>
2025-08-20 17:27:46 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main () {
+    printf ("Disciplina: Algoritmo e programação estruturada\n");
+    printf ("Turma: GPE17N20241\n");
+    printf ("Nome: Danylo Rodrigues da Silva\n");
+    printf ("Matrícula: UC25101463\n");
+
+    return 0;
+}
